56f349c3a3b6bb57870302c03516be3e9e4fade1
devgiljong <calf-love@naver.com>
2023-11-17 00:14:35 +0900

    :sparkles: 히스토리 플러그인

--- core/stackflow.ts
+++ core/stackflow.ts
@@ -1,6 +1,7 @@
 import Article from '@/components/Article';
 import MyActivity from '@/components/MyActivity';
 import { basicUIPlugin } from '@stackflow/plugin-basic-ui';
+import { historySyncPlugin } from '@stackflow/plugin-history-sync';
 import { basicRendererPlugin } from '@stackflow/plugin-renderer-basic';
 import { stackflow } from '@stackflow/react';
 
